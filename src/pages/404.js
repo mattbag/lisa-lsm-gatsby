fdf5187332c1d859e85ae1727cc04f262c587862
mattbag <wised@AirMatt.local>
2018-01-20 17:51:29 +1100

    404 loc check

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,10 +2,13 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import pic11 from '../assets/images/pic11.jpg'
+import pic from '../assets/images/made-in-italy.jpg'
 
 const Page404 = (props) => {
-    location.href = '/'
+    // console.log(props);
+    
+    if (location) location.href = '/'
+
     return (
     <div>
         <Helmet>
@@ -19,10 +22,10 @@ const Page404 = (props) => {
                     <header className="major">
                         <h1>404</h1>
                     </header>
-                    <span className="image main"><img src={pic11} alt="" /></span>
                     <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
                     <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+                    <span className="image main"><img src={pic} alt="" /></span>
                 </div>
             </section>
         </div>
