784598fbbb5fb5d2c1aa8bff1dc2529882fd9b73
mattbag <wised@AirMatt.local>
2018-01-21 10:11:36 +1100

    indes content

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,11 @@ import Banner from '../components/Banner'
 
 import pic01 from '../assets/images/bg_1.jpg'
 import pic02 from '../assets/images/bg_2.jpg'
-import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
-import pic05 from '../assets/images/pic05.jpg'
-import pic06 from '../assets/images/pic06.jpg'
+import pic03 from '../assets/images/bg_3.jpg'
+// import pic03 from '../assets/images/pic03.jpg'
+// import pic04 from '../assets/images/pic04.jpg'
+// import pic05 from '../assets/images/pic05.jpg'
+// import pic06 from '../assets/images/pic06.jpg'
 
 class HomeIndex extends React.Component {
     render() {
@@ -26,33 +27,59 @@ class HomeIndex extends React.Component {
 
                 <div id="main" style={{maxWidth:`100rem`,margin:`0 auto`}}>
                 <section id="two" className="spotlights">
-                <section>
-                {/* <Link to="/generic" className="image" style={{backgroundImage: `url(${pic01})`}}> */}
-                <Link to="/generic" className="image">
-                    <img src={pic01} alt="" />
-                </Link>
-                <div className="content">
-                    <div className="inner">
-                        <header className="major">
-                            <h3>Custom Fashion Label</h3>
-                        </header>
-                        <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                        {/* <ul className="actions">
-                            <li><Link to="/generic" className="button">Learn more</Link></li>
-                        </ul> */}
+
+                    <section>
+                        <Link to="/generic" className="image">
+                            <img src={pic03} alt="" />
+                        </Link>
+                        <div className="content">
+                            <div className="inner">
+                                <header className="major">
+                                    <h3>Mettiamo tutta la nostra competenza a vostra disposizione</h3>
+                                </header>
+                                <p>Ascoltiamo le vostre idee e, discutendo insieme, vi aiutiamo a concretizzare i vostri progetti. 
+Partendo dai vostri input o prendendo spunto dai nostri campionari già creati, sviluppiamo e diamo forma e colore ai vostri prototipi portando alla lune la vostra idea di maglia. 
+</p>
+                                {/* <ul className="actions">
+                                    <li><Link to="/generic" className="button">Learn more</Link></li>
+                                </ul> */}
+                            </div>
+                        </div>
+                    </section>
+
+                    <section>
+                    {/* <Link to="/generic" className="image" style={{backgroundImage: `url(${pic01})`}}> */}
+                    <Link to="/generic" className="image">
+                        <img src={pic01} alt="" />
+                    </Link>
+                    <div className="content">
+                        <div className="inner">
+                            <header className="major">
+                                <h3>Competenza e qualità</h3>
+                            </header>
+<p>Siamo in grado di produrre ogni genere di maglia, sia essa sia calata, tagliata, ad intarsio o jacquard.
+La ricerca dei materiali di qualità, sapientemente mixata alle idee, unite alla precisione artigianale da’ origine ad un qualcosa di unico: l’avanguardia industriale dell’artigianalità.
+La personalizzazione è un passaggio fondamentale nella genesi del vostro prodotto, step che ne esalta in ogni aspetto le possibilità di riuscita dei vostri progetti. 
+</p>
+                            {/* <ul className="actions">
+                                <li><Link to="/generic" className="button">Learn more</Link></li>
+                            </ul> */}
+                        </div>
                     </div>
-                </div>
-            </section>
-            <section>
+                </section>
+
+                <section>
                     <Link to="/generic" className="image">
                         <img src={pic02} alt="" />
                     </Link>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Second Topic</h3>
+                                <h3>Il capitale umano</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+<p>I nostri prodotti provengono dal lavoro e dalle conoscenze di professionisti e operatori motivati e competenti. Collaboratori diretti e indiretti, con i quali costruiamo rapporti di partnership costanti e duraturi basati sulla fiducia, capaci di trasformare semplici idee e materiali in splendide creazioni di maglia. Tutta la nostra produzione nasce dalla nostra terra, non abbiamo bisogno di importare o delocalizzare nulla dato che il distretto tessile di Carpi può vantare una delle migliori specializzazioni al mondo e noi ne siamo parte integrante. 
+I nostri prodotti sono tutti rigorosamente Made in Italy.
+</p>
                             {/* <ul className="actions">
                                 <li><Link to="/generic" className="button">Learn more</Link></li>
                             </ul> */}
