dee22b234586784dda4855ee829b753f67ff334e
mattbag <wised@AirMatt.local>
2018-01-21 11:21:33 +1100

    pitti

--- src/pages/index.js
+++ src/pages/index.js
@@ -118,6 +118,30 @@ Nato nel 1986 a Carpi, il distretto italiano da sempre tra i più prolifici per 
                             </ul> */}
                         </div>
                     </section>
+
+                    <section>
+                        <hr style={{borderBottomColor: `#000`, margin: `2rem auto 0 auto`, width:`20%`}}/>
+                        <div className="inner" style={{textAlign:`center`,maxWidth:`50rem` ,paddingRight:`2rem`,paddingLeft:`2rem`}}>
+                            <header className="">
+                                <h2>PITTI IMMAGINE FILATI</h2>
+                            </header>
+                                <p>Puoi trovare le nostre creazioni all'interno di <br/>
+                                <b>Pitti Immagine Filati 82</b>
+                                <br/>
+                                24-26 Gennaio 2018
+                                <br/>
+                                stand Shima Seiki c-18/19
+                                <br/>
+                                Padiglione Centrale - piano inferiore
+                                <br/>
+                                Fortezza da Basso - Firenze
+                                </p>
+                            {/* <ul className="actions">
+                                <li><Link to="/landing" className="button next">Come along</Link></li>
+                            </ul> */}
+                        </div>
+                            <hr style={{borderBottomColor: `#000`, margin: `1rem auto`, width:`20%`}}/>
+                    </section>
                 </div>
 
             </div>
