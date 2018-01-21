185bf13c8862389a0d7691a41a4706660ce7da2f
mattbag <wised@AirMatt.local>
2018-01-21 11:13:22 +1100

    blocks

---
+++ src/components/Block.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import pic01 from '../assets/images/bg_1.jpg'
+
+const Block = (props) => (
+    <article className="block">
+        <div className="block__top">
+            {/* <Link to="/generic" className="image"> */}
+                <img src={props.img} alt={props.title} width="100%" />
+            {/* </Link> */}
+        </div>
+        <div className="block__copy sha">
+            <div className="content">
+                <div className="inner">
+                    <header className="major">
+                    <h3>{props.title}</h3>
+                        {/* <h3>Mettiamo tutta la nostra competenza a vostra disposizione</h3> */}
+                    </header>
+                    {/* <p>Ascoltiamo le vostre idee e, discutendo insieme, vi aiutiamo a concretizzare i vostri progetti.
+                    Partendo dai vostri input o prendendo spunto dai nostri campionari già creati, sviluppiamo e diamo forma e colore ai vostri prototipi portando alla lune la vostra idea di maglia.
+                    </p> */}
+                    <div dangerouslySetInnerHTML={{__html:props.copy}} />
+                    {/* <ul className="actions">
+            <li><Link to="/generic" className="button">Learn more</Link></li>
+        </ul> */}
+                </div>
+            </div>
+        </div>
+    </article>
+)
+
+export default Block
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import Banner from '../components/Banner'
+import Block from '../components/Block'
 
 import pic01 from '../assets/images/bg_1.jpg'
 import pic02 from '../assets/images/bg_2.jpg'
@@ -26,6 +27,22 @@ class HomeIndex extends React.Component {
                 <Banner />
 
                 <div id="main" style={{maxWidth:`100rem`,margin:`0 auto`}}>
+                <Block img={pic01} title={'Mettiamo tutta la nostra competenza a vostra disposizione'} copy={`
+                   <p>Ascoltiamo le vostre idee e, discutendo insieme, vi aiutiamo a concretizzare i vostri progetti. 
+                   Partendo dai vostri input o prendendo spunto dai nostri campionari già creati, sviluppiamo e diamo forma e colore ai vostri prototipi portando alla lune la vostra idea di maglia. 
+                   </p>
+                `}/>
+                <Block img={pic02} title={'Competenza e qualità'} copy={`
+                <p>Siamo in grado di produrre ogni genere di maglia, sia essa sia calata, tagliata, ad intarsio o jacquard.
+                La ricerca dei materiali di qualità, sapientemente mixata alle idee, unite alla precisione artigianale da’ origine ad un qualcosa di unico: l’avanguardia industriale dell’artigianalità.
+                La personalizzazione è un passaggio fondamentale nella genesi del vostro prodotto, step che ne esalta in ogni aspetto le possibilità di riuscita dei vostri progetti. 
+                </p>
+                `}/>
+                <Block img={pic03} title={'Il capitale umano'} copy={`
+               <p>I nostri prodotti provengono dal lavoro e dalle conoscenze di professionisti e operatori motivati e competenti. Collaboratori diretti e indiretti, con i quali costruiamo rapporti di partnership costanti e duraturi basati sulla fiducia, capaci di trasformare semplici idee e materiali in splendide creazioni di maglia. Tutta la nostra produzione nasce dalla nostra terra, non abbiamo bisogno di importare o delocalizzare nulla dato che il distretto tessile di Carpi può vantare una delle migliori specializzazioni al mondo e noi ne siamo parte integrante. 
+               I nostri prodotti sono tutti rigorosamente Made in Italy.
+               </p>
+               `}/>
                 <section id="two" className="spotlights">
 
                     <section>
@@ -88,8 +105,8 @@ I nostri prodotti sono tutti rigorosamente Made in Italy.
                 </section>
             </section>
 
-                    <section id="two">
-                        <div className="inner" style={{maxWidth:`40rem`}}>
+                    <section>
+                        <div className="inner sha" style={{maxWidth:`40rem` ,paddingRight:`2rem`,paddingLeft:`2rem`}}>
                             <header className="major">
                                 <h2>Storia</h2>
                             </header>
